678218bbf7f0eb23a9d6b0dde263445474e3ee8f
Kyrael123 <113521321+Kyrael123@users.noreply.github.com>
2023-01-20 19:51:03 +0100

    SQL-Skript Changes

--- SQL-Skript.sql
+++ SQL-Skript.sql
@@ -1,26 +1,26 @@
-﻿drop database if exists ClickA_DB;
-
-go
-create database ClickA_DB;
+﻿if not exists (select * from sys.databases where name = 'ClickA_DB')
+BEGIN
+create DATABASE ClickA_DB;
+end
 
 go
 use ClickA_DB;
 
-go
-create table Users (
-id int,
-username char(20)
-)
+drop TABLE if EXISTS  nutzer;
 
 go
-insert into Users values
-(1,'Testuser1'),
-(2,'Testuser2'),
-(3,'Testuser3'),
-(4,'Testuser4'),
-(5,'Testuser5'),
-(6,'Testuser6'),
-(7,'Testuser7'),
-(8,'Testuser8'),
-(9,'Testuser9'),
-(10,'Testuser10');
+create table nutzer (
+id int,
+username char(20),
+password char(20),
+energy int,
+clickpower int,
+generator int,
+transformer int,
+clickbotfabrik int,
+clickbot int
+);
+
+go
+insert into nutzer values
+(1,'Testuser1','test',0,0,0,0,0,0);
